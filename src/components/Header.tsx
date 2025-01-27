552215a53d3afad9c6db1dc0cde055fda4c01213
Ataman-Front-Mahdi <atamanorum@gmail.com@gmail.com>
2025-01-27 05:07:55 -0800

    add some useless code

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="py-5 bg-blue-500">
+      <div className="flex"></div>
+      <div className="flex"></div>
+    </header>
+  )
+}
